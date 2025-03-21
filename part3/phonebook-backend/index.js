52a8bf6ebdaba8223108a6d89bac49f0ab91c3db
lucian popa <lucianpopa98@yahoo.com>
2025-03-21 09:03:05 +0200

    readded cors because extensions interferring

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 app.use(express.static('dist'))
 
 // not a good practice to show data in console
