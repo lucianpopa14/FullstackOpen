9efe0b2ea314cfbb8d8f473a5622b643ce06dfe3
lucian popa <lucianpopa98@yahoo.com>
2025-03-21 08:32:34 +0200

    whole app on the internet

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static('dist'))
 
 // not a good practice to show data in console
 // morgan.token('body', (req) => {
