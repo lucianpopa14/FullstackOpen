20c6a8832cb9da2858d86977d324f7fc27026145
lucian popa <lucianpopa98@yahoo.com>
2025-04-07 09:00:29 +0300

    modified routes to use database

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -20,89 +20,77 @@ app.use(express.static('dist'))
 
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-];
-
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons =>{
-        response.json(persons)
-    })
-})
+    Person.find({}).then(persons => {
+        response.json(persons);
+    }).catch(error => {
+        response.status(500).json({ error: 'Internal Server Error' });
+    });
+});
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
+    const id = request.params.id;
+    Person.findById(id).then(person => {
+        if (person) {
+            response.json(person);
+        } else {
+            response.status(404).end();
+        }
+    }).catch(error => {
+        response.status(500).json({ error: 'Internal Server Error' });
+    });
+});
 
 app.get('/info', (request, response) => {
-    const peopleNumber = persons.length;
-    const date = new Date();
-    const formattedDate = date.toLocaleString('en-GB', { timeZone: 'Europe/Bucharest' });
-    response.send(`<p>Phonebook has info for ${peopleNumber} people</p> <p>${formattedDate}</p>`);
-})
+    Person.find({}).then(persons => {
+        const peopleNumber = persons.length;
+        const date = new Date();
+        const formattedDate = date.toLocaleString('en-GB', { timeZone: 'Europe/Bucharest' });
+        response.send(`<p>Phonebook has info for ${peopleNumber} people</p> <p>${formattedDate}</p>`);
+    }).catch(error => {
+        response.status(500).json({ error: 'Internal Server Error' });
+    });
+});
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-})
-
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => Number(n.id)))
-        : 0
-    return String(maxId + 1)
-}
+    const id = request.params.id;
+    Person.findByIdAndDelete(id).then(() => {
+        response.status(204).end();
+    }).catch(error => {
+        response.status(500).json({ error: 'Internal Server Error' });
+    });
+});
 
 app.post('/api/persons', (request, response) => {
-    const body = request.body
+    const body = request.body;
 
     if (!body.name || !body.number) {
         return response.status(400).json({
             error: 'name or number missing'
-        })
+        });
     }
 
-    const alreadyExists = persons.find(person => person.name === body.name)
-    if (alreadyExists) {
-        return response.status(409).json({
-            error: 'name must be unique'
-        })
-    }
+    Person.findOne({ name: body.name }).then(existingPerson => {
+        if (existingPerson) {
+            return response.status(409).json({
+                error: 'name must be unique'
+            });
+        }
 
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId()
-    }
-    persons = [...persons, person]
-    response.json(person)
-})
+        const person = new Person({
+            name: body.name,
+            number: body.number
+        });
+
+        person.save().then(savedPerson => {
+            response.json(savedPerson);
+        }).catch(error => {
+            response.status(500).json({ error: 'Internal Server Error' });
+        });
+    }).catch(error => {
+        response.status(500).json({ error: 'Internal Server Error' });
+    });
+});
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
