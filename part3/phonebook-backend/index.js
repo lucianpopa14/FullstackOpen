9f5fd7f42c831ca5be85c3e9cc5ec455abe27acf
lucian popa <lucianpopa98@yahoo.com>
2025-04-07 09:42:11 +0300

    exercises 3.13-3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,34 +1,22 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
-const Person = require('./models/person')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const Person = require('./models/person');
+require('dotenv').config();
 
-app.use(express.json())
-app.use(morgan('tiny'))
-app.use(cors())
-app.use(express.static('dist'))
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.static('dist'));
 
-// not a good practice to show data in console
-// morgan.token('body', (req) => {
-//     if (req.method === 'POST' && req.body && req.body.name && req.body.number) {
-//         return `{name: ${req.body.name}, number: ${req.body.number}}`;
-//     }
-//     return '';
-// });
-
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(persons => {
         response.json(persons);
-    }).catch(error => {
-        response.status(500).json({ error: 'Internal Server Error' });
-    });
+    }).catch(error => next(error));
 });
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id;
     Person.findById(id).then(person => {
         if (person) {
@@ -36,32 +24,26 @@ app.get('/api/persons/:id', (request, response) => {
         } else {
             response.status(404).end();
         }
-    }).catch(error => {
-        response.status(500).json({ error: 'Internal Server Error' });
-    });
+    }).catch(error => next(error));
 });
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     Person.find({}).then(persons => {
         const peopleNumber = persons.length;
         const date = new Date();
         const formattedDate = date.toLocaleString('en-GB', { timeZone: 'Europe/Bucharest' });
         response.send(`<p>Phonebook has info for ${peopleNumber} people</p> <p>${formattedDate}</p>`);
-    }).catch(error => {
-        response.status(500).json({ error: 'Internal Server Error' });
-    });
+    }).catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     const id = request.params.id;
     Person.findByIdAndDelete(id).then(() => {
         response.status(204).end();
-    }).catch(error => {
-        response.status(500).json({ error: 'Internal Server Error' });
-    });
+    }).catch(error => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body;
 
     if (!body.name || !body.number) {
@@ -84,15 +66,44 @@ app.post('/api/persons', (request, response) => {
 
         person.save().then(savedPerson => {
             response.json(savedPerson);
-        }).catch(error => {
-            response.status(500).json({ error: 'Internal Server Error' });
-        });
-    }).catch(error => {
-        response.status(500).json({ error: 'Internal Server Error' });
-    });
+        }).catch(error => next(error));
+    }).catch(error => next(error));
 });
 
-const PORT = process.env.PORT
+app.put('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id;
+    const body = request.body;
+
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        });
+    }
+
+    Person.findByIdAndUpdate(id, { number: body.number }, { new: true, runValidators: true })
+        .then(updatedPerson => {
+            if (updatedPerson) {
+                response.json(updatedPerson);
+            } else {
+                response.status(404).json({ error: 'Person not found' });
+            }
+        })
+        .catch(error => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    }
+
+    response.status(500).json({ error: 'Internal Server Error' });
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
