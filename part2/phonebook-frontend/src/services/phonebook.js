0c3269a2cde7da65924827d94ced933505ea70a2
lucian popa <lucianpopa98@yahoo.com>
2025-04-08 20:03:00 +0300

    updated frontend service

--- part2/phonebook-frontend/src/services/phonebook.js
+++ part2/phonebook-frontend/src/services/phonebook.js
@@ -4,12 +4,7 @@ const baseUrl = 'https://fullstackopen-k6l1.onrender.com/api/persons';
 const getAll = () => {
     console.log('Requesting data from:', baseUrl); // Log the URL being requested
     const request = axios.get(baseUrl);
-    const nonExisting = {
-        id: 10000,
-        content: 'This phonebook entry is not saved to server',
-        important: true,
-    };
-    return request.then((response) => response.data.concat(nonExisting));
+    return request.then((response) => response.data);
 };
 
 
