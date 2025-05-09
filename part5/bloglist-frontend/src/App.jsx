192a65046983154c3753dc956ac74d831b62fa64
lucian popa <lucianpopa98@yahoo.com>
2025-05-09 09:31:36 +0300

    ex 5.10 sorting by number of likes

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -138,8 +138,10 @@ const App = () => {
           handleSubmit={handleBlogSubmit}
         />
       </Togglable>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} onUpdate={refreshBLog}/>
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} onUpdate={refreshBLog}/>
       )}
     </div>
   )
