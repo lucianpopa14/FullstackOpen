62c8785732309e5e482f4bff038a94147104a0ed
lucian popa <lucianpopa98@yahoo.com>
2025-05-28 09:23:31 +0300

    indent

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -35,7 +35,7 @@ const reducer = (state = initialState, action) => {
         anecdote.id !== id ? anecdote : changedAnecdote
       )
     }
-    case 'NEW_ANECDOTE':{
+    case 'NEW_ANECDOTE': {
       return [...state, asObject(action.payload)]
     }
     default:
